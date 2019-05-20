d9bd341ca9b9c0742290970ca7258d1f2163fe99
Justin Harr <justin@Justins-MBP.lan>
2019-05-20 18:11:08 -0400

    update keys

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,10 @@
 //Prod keys
 module.exports={
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  
     mongoUri: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
-  sendGridAPI: process.env.SEND_GRID_API
-
-}
+  sendGridAPI: process.env.SEND_GRID_API,
+  awsAccessKey: process.env.AWS_ACCESS_KEY,
+  awsSecretKey: process.env.AWS_SECRET_KEY,
+  awsBucket: process.env.AWS_BUCKET,
+ }
